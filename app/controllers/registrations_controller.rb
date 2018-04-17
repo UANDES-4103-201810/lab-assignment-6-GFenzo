11c58db332a6ae2eda63cbde893b5a2db52e5dba
Web <fjmoreira@miuandes.cl>
2018-04-17 13:38:58 -0300

    added registration method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,18 @@ class RegistrationsController < ApplicationController
 	end
 
 	def create
-	    #complete this method
+		if required_user_params != ""
+			@user = User.new(required_user_params)
+			if @user.save
+				redirect_to root_path
+			end
+		else
+			flash[:notice] = "It failed, missing parameters"
+			redirect_to sign_in_path
+		end
+	end
+
+	def required_user_params
+		params.require(:registrations).permit(:name, :last_name, :email, :password, :phone)
 	end
 end
